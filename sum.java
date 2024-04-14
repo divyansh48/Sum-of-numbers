90f91b5748be5a5a5117177bc48776a34af9d1d4
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-14 11:07:32 +0530

    sum.java is added

---
+++ sum.java
@@ -0,0 +1,22 @@
+package com.divyansh;
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("enter the first number :");
+        int num1 = input.nextInt();
+        System.out.print("enter the second number :");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2 ;
+        System.out.println("the sum of the numbers is:" + sum);
+
+
+
+
+
+    }
+}
